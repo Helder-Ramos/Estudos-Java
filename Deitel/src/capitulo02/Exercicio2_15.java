5a3f8dd0ea9ba4d234b56594f63539ba3ff68737
Helder de Sousa Ramos <helder.sramos@gmail.com>
2026-04-19 11:13:15 -0300

    Estrutura inicial: Projeto Deitel Java

---
+++ Deitel/src/capitulo02/Exercicio2_15.java
@@ -0,0 +1,13 @@
+package capitulo02;
+
+/**
+ *
+ * @author Helder
+ */
+public class Exercicio2_15
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Olá, Mundo! Este é o exercício 2.15.");
+    }
+}
